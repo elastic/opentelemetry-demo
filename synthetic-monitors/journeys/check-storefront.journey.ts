1cbbabacc6cc7181a3dd21bacb4b16e50ed1a1f0
Andrew Cholakian <andrew@andrewvc.com>
2024-10-14 14:56:39 -0500

    Synth fixes

--- synthetic-monitors/journeys/check-storefront.journey.ts
+++ synthetic-monitors/journeys/check-storefront.journey.ts
@@ -1,10 +1,7 @@
 import { journey, step, monitor, expect } from "@elastic/synthetics";
 
 journey("Check storefront homepage", ({ page, params }) => {
-  // Only relevant for the push command to create
-  // monitors in Kibana
   monitor.use({
-    id: "example-monitor",
     schedule: 10,
   });
   step("launch application", async () => {
