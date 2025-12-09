ec38fc165bdf6da8cda9fdaf5f55abfef03b0b03
Dónal O'Sullivan <donal.osullivan@elastic.co>
2025-12-09 11:59:58 +0000

    fix: Put string on one line as this was causing compliation issues, this was from the merge with upstream

--- src/cart/src/Program.cs
+++ src/cart/src/Program.cs
@@ -73,8 +73,7 @@ app.MapGrpcHealthChecksService();
 
 app.MapGet("/", async context =>
 {
-    await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.
-com/fwlink/?linkid=2086909");
+    await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 });
 
 app.Run();
