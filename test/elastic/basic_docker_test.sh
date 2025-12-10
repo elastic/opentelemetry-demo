a60011fa0263919da578f7e126a513d750a32919
Dónal O'Sullivan <donal.osullivan@elastic.co>
2025-12-10 13:41:59 +0000

    Update bashunit docker tests to use custom assertions, this will allow test failure information to be shown

--- test/elastic/basic_docker_test.sh
+++ test/elastic/basic_docker_test.sh
@@ -12,19 +12,59 @@ function tear_down_after_script() {
   uninstall_local_elastic_stack
 }
 
-function test_launch_demo_docker() { 
-  launch_demo "cloud-hosted" "docker"
+function assert_docker_service_running() {
+  local service="$1"
+
+  if ! check_docker_service_running "$service"; then
+    local status=$(docker ps --filter "name=^${service}$" --format '{{.Status}}' 2>/dev/null)
+    local container_info=$(docker ps -a --filter "name=^${service}$" --format '{{.Status}}\t{{.Image}}' 2>/dev/null)
+
+    if [[ -n "$container_info" ]]; then
+      bashunit::assertion_failed "service '$service' to be running" "status: ${status:-not found}, details: $container_info" "got"
+    else
+      bashunit::assertion_failed "service '$service' to be running" "container does not exist" "got"
+    fi
+    return
+  fi
+
+  bashunit::assertion_passed
 }
 
-function test_check_docker_service_running() { 
+function assert_demo_launched() {
+  local deployment_type="$1"
+  local platform="$2"
+
+  if ! launch_demo "$deployment_type" "$platform"; then
+    bashunit::assertion_failed "demo to launch successfully on $platform with $deployment_type" "launch failed" "got"
+    return
+  fi
+
+  bashunit::assertion_passed
+}
+
+function assert_demo_destroyed() {
+  local platform="$1"
+
+  if ! destroy_demo "$platform"; then
+    bashunit::assertion_failed "demo to be destroyed on $platform" "destruction failed" "got"
+    return
+  fi
+
+  bashunit::assertion_passed
+}
+
+function test_launch_demo_docker() {
+  assert_demo_launched "cloud-hosted" "docker"
+}
+
+function test_check_docker_service_running() {
   local services=($(docker compose config --services))
 
   for service in "${services[@]}"; do
-    assert_exit_code "0" "$(check_docker_service_running "$service")"
+    assert_docker_service_running "$service"
   done
 }
 
-function test_destroy_demo_docker() { 
-  destroy_demo "docker"
+function test_destroy_demo_docker() {
+  assert_demo_destroyed "docker"
 }
-
