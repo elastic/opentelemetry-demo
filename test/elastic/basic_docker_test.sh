a034df02caf426c2915ca5371eeb8616d3aa5347
Dónal O'Sullivan <donal.osullivan@elastic.co>
2025-12-10 10:20:03 +0000

    Update bash unit test for the docker services to use a list of docker services from docker compose instead of having to maintain a separate list

--- test/elastic/basic_docker_test.sh
+++ test/elastic/basic_docker_test.sh
@@ -17,34 +17,7 @@ function test_launch_demo_docker() {
 }
 
 function test_check_docker_service_running() { 
-  local services=(
-    "accounting"
-    "ad"
-    "cart"
-    "checkout"
-    "currency"
-    "email"
-    "fraud-detection"
-    "frontend"
-    "frontend-proxy"
-    "image-provider"
-    "load-generator"
-    "payment"
-    "product-catalog"
-    "quote"
-    "recommendation"
-    "shipping"
-    "flagd"
-    "flagd-ui"
-    "kafka"
-    "postgresql"
-    "valkey-cart"
-    "jaeger"
-    "grafana"
-    "otel-collector"
-    "prometheus"
-    "opensearch"
-  )
+  local services=($docker compose config --services)
 
   for service in "${services[@]}"; do
     assert_exit_code "0" "$(check_docker_service_running "$service")"
