63d930c1e73635d2b286cd937d7d01695280d0e1
Dónal O'Sullivan <donal.osullivan@elastic.co>
2025-12-10 13:40:46 +0000

    Update bashunit k8s tests to use custom assertions, this will allow test failure information to be shown

--- test/elastic/basic_k8s_test.sh
+++ test/elastic/basic_k8s_test.sh
@@ -12,13 +12,33 @@ function tear_down_after_script() {
   uninstall_local_elastic_stack
 }
 
+function assert_demo_launched() {
+  local deployment_type="$1"
+  local platform="$2"
+
+  if ! launch_demo "$deployment_type" "$platform"; then
+    bashunit::assertion_failed "demo to launch successfully on $platform with $deployment_type" "launch failed" "got"
+    return
+  fi
+
+  bashunit::assertion_passed
+}
+
+function assert_demo_destroyed() {
+  local platform="$1"
+
+  if ! destroy_demo "$platform"; then
+    bashunit::assertion_failed "demo to be destroyed on $platform" "destruction failed" "got"
+    return
+  fi
+
+  bashunit::assertion_passed
+}
+
 function test_launch_demo_k8s() {
-  echo "=== DEBUG: Starting K8s test ==="
-  echo "About to call launch_demo with cloud-hosted k8s"
-  launch_demo "cloud-hosted" "k8s"
-  echo "=== DEBUG: K8s test completed ==="
+  assert_demo_launched "cloud-hosted" "k8s"
 }
 
 function test_destroy_demo_k8s() {
-  destroy_demo "k8s"
+  assert_demo_destroyed "k8s"
 }
