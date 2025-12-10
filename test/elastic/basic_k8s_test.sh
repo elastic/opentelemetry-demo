28e4ab068b4b5c19c9a765d77754f8c1e5dd4895
Dónal O'Sullivan <donal.osullivan@elastic.co>
2025-12-10 11:50:59 +0000

    Add debug statements to k8s test

--- test/elastic/basic_k8s_test.sh
+++ test/elastic/basic_k8s_test.sh
@@ -12,11 +12,13 @@ function tear_down_after_script() {
   uninstall_local_elastic_stack
 }
 
-function test_launch_demo_k8s() { 
+function test_launch_demo_k8s() {
+  echo "=== DEBUG: Starting K8s test ==="
+  echo "About to call launch_demo with cloud-hosted k8s"
   launch_demo "cloud-hosted" "k8s"
+  echo "=== DEBUG: K8s test completed ==="
 }
 
-function test_destroy_demo_k8s() { 
+function test_destroy_demo_k8s() {
   destroy_demo "k8s"
 }
-
